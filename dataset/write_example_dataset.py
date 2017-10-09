c1509270e3a9bc336e761472301af2801da8ff50
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-09 13:01:25 +0200

    run yapf

--- dataset/write_example_dataset.py
+++ dataset/write_example_dataset.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python
 
 import ROOT
-ROOT.PyConfig.IgnoreCommandLineOptions = True # disable ROOT internal argument parser
+ROOT.PyConfig.IgnoreCommandLineOptions = True  # disable ROOT internal argument parser
 import argparse
 import yaml
 import numpy as np
 import os
 from array import array
 
-np.random.seed(1234) # makes the output files reproducible
+np.random.seed(1234)  # makes the output files reproducible
+
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Write example dataset used for testing the scripts")
+    parser = argparse.ArgumentParser(
+        description="Write example dataset used for testing the scripts")
     parser.add_argument("output_path", help="Path to output files")
     return parser.parse_args()
+
 
 def main(args):
     # Processes, which contribute to the dataset
     # The observables of the processes are the variables "x" and "y", which follow a Gaussian 2D distributions with mean "mean", unity variance and correlation "corr".
-    processes = {"signal":{"num_entries":1000, "norm":0.1, "mean":[0.0, 0.0], "corr":0.0}
-            ,"background_1":{"num_entries":1000, "norm":0.6, "mean":[0.5, 0.5], "corr":-0.7},
-            "background_2":{"num_entries":1000, "norm":0.3, "mean":[-0.5, -0.5], "corr":0.7}}
+    processes = {
+        "signal": {
+            "num_entries": 1000,
+            "norm": 0.1,
+            "mean": [0.0, 0.0],
+            "corr": 0.0
+        },
+        "background_1": {
+            "num_entries": 1000,
+            "norm": 0.6,
+            "mean": [0.5, 0.5],
+            "corr": -0.7
+        },
+        "background_2": {
+            "num_entries": 1000,
+            "norm": 0.3,
+            "mean": [-0.5, -0.5],
+            "corr": 0.7
+        }
+    }
 
     # Write Monte Carlo-like output files
     for process in processes:
@@ -34,13 +54,18 @@ def main(args):
         weights = array('f', [-999])
         tree.Branch('weights', weights, 'weights/F')
 
-        covariance_matrix = [[1.0, processes[process]["corr"]], [processes[process]["corr"], 1.0]]
-        values = np.random.multivariate_normal(processes[process]["mean"], covariance_matrix, size=processes[process]["num_entries"])
+        covariance_matrix = [[1.0, processes[process]["corr"]],
+                             [processes[process]["corr"], 1.0]]
+        values = np.random.multivariate_normal(
+            processes[process]["mean"],
+            covariance_matrix,
+            size=processes[process]["num_entries"])
 
         for value in values:
             x[0] = value[0]
             y[0] = value[1]
-            weights[0] = processes[process]["norm"]/float(processes[process]["num_entries"])
+            weights[0] = processes[process]["norm"] / float(
+                processes[process]["num_entries"])
             tree.Fill()
 
         file_.Write()
@@ -56,8 +81,13 @@ def main(args):
     tree.Branch('y', y, 'y/F')
 
     for process in processes:
-        covariance_matrix = [[1.0, processes[process]["corr"]], [processes[process]["corr"], 1.0]]
-        values = np.random.multivariate_normal(processes[process]["mean"], covariance_matrix, size=int(processes[process]["num_entries"]*processes[process]["norm"]))
+        covariance_matrix = [[1.0, processes[process]["corr"]],
+                             [processes[process]["corr"], 1.0]]
+        values = np.random.multivariate_normal(
+            processes[process]["mean"],
+            covariance_matrix,
+            size=int(processes[process]["num_entries"] *
+                     processes[process]["norm"]))
 
         for value in values:
             x[0] = value[0]
@@ -66,6 +96,7 @@ def main(args):
 
     file_.Write()
     file_.Close()
+
 
 if __name__ == "__main__":
     args = parse_arguments()
