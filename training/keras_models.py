66dded59a4a10bbda4d2031938e79243adba9270
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2017-10-30 16:00:50 +0100

    add correct MA keras model

--- training/keras_models.py
+++ training/keras_models.py
@@ -1,6 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.regularizers import l2
 
 
 def example(num_inputs, num_outputs):
@@ -25,13 +26,13 @@ def smhtt(num_inputs, num_outputs):
     model = Sequential()
     model.add(
         Dense(
-            300, init="glorot_normal", activation="relu",
+            300, init="glorot_normal", activation="relu", W_regularizer=l2(1e-4),
             input_dim=num_inputs))
-    model.add(Dense(300, init="glorot_normal", activation="relu"))
-    model.add(Dense(300, init="glorot_normal", activation="relu"))
+    model.add(Dense(300, init="glorot_normal", activation="relu", W_regularizer=l2(1e-4)))
+    model.add(Dense(300, init="glorot_normal", activation="relu", W_regularizer=l2(1e-4)))
     model.add(Dense(num_outputs, init="glorot_uniform", activation="softmax"))
     model.compile(
-        loss="categorical_crossentropy",
+        loss="mean_squared_error",
         optimizer=Adam(),
         metrics=[
             "categorical_accuracy",
