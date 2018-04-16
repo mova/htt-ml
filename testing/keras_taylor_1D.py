83e13401c32eaf26c44f1b3ec5cde0df728b3438
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2018-04-16 15:24:07 +0200

    add testing script for 1D taylor coefficients

---
+++ testing/keras_taylor_1D.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True  # disable ROOT internal argument parser
+
+import argparse
+from array import array
+import yaml
+import pickle
+import numpy as np
+import os
+import sys
+
+import matplotlib as mpl
+mpl.use('Agg')
+mpl.rcParams['font.size'] = 16
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from keras.models import load_model
+import tensorflow as tf
+
+from tensorflow_derivative.inputs import Inputs
+from tensorflow_derivative.outputs import Outputs
+from tensorflow_derivative.derivatives import Derivatives
+
+import logging
+logger = logging.getLogger("keras_taylor_1D")
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Produce confusion matrice")
+    parser.add_argument("config_training", help="Path to training config file")
+    parser.add_argument("config_testing", help="Path to testing config file")
+    parser.add_argument("fold", type=int, help="Trained model to be tested.")
+    return parser.parse_args()
+
+
+def parse_config(filename):
+    logger.debug("Load config %s.", filename)
+    return yaml.load(open(filename, "r"))
+
+
+def main(args, config_test, config_train):
+    # Load preprocessing
+    path = os.path.join(config_train["output_path"],
+                        config_test["preprocessing"][args.fold])
+    logger.info("Load preprocessing %s.", path)
+    preprocessing = pickle.load(open(path, "rb"))
+
+    # Load Keras model
+    path = os.path.join(config_train["output_path"],
+                        config_test["model"][args.fold])
+    logger.info("Load keras model %s.", path)
+    model_keras = load_model(path)
+
+    # Get TensorFlow graph
+    inputs = Inputs(config_train["variables"])
+
+    try:
+        sys.path.append("htt-ml/training")
+        import keras_models
+    except:
+        logger.fatal("Failed to import Keras models.")
+        raise Exception
+    try:
+        name_keras_model = config_train["model"]["name"]
+        model_tensorflow_impl = getattr(
+            keras_models, config_train["model"]["name"] + "_tensorflow")
+    except:
+        logger.fatal(
+            "Failed to load TensorFlow version of Keras model {}.".format(
+                name_keras_model))
+        raise Exception
+
+    model_tensorflow = model_tensorflow_impl(inputs.placeholders, model_keras)
+    outputs = Outputs(model_tensorflow, config_train["classes"])
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    # Get operations for first-order derivatives
+    deriv_ops = {}
+    derivatives = Derivatives(inputs, outputs)
+    for class_ in config_train["classes"]:
+        deriv_ops[class_] = []
+        for variable in config_train["variables"]:
+            deriv_ops[class_].append(derivatives.get(class_, [variable]))
+
+    # Loop over testing dataset
+    path = os.path.join(config_train["datasets"][(1, 0)[args.fold]])
+    logger.info("Loop over test dataset %s to get model response.", path)
+    file_ = ROOT.TFile(path)
+    mean_abs_deriv = {}
+    for i_class, class_ in enumerate(config_train["classes"]):
+        logger.debug("Process class %s.", class_)
+
+        tree = file_.Get(class_)
+        if tree == None:
+            logger.fatal("Tree %s does not exist.", class_)
+            raise Exception
+        values = []
+        for variable in config_train["variables"]:
+            values.append(array("f", [-999]))
+            tree.SetBranchAddress(variable, values[-1])
+        weight = array("f", [-999])
+        tree.SetBranchAddress(config_test["weight_branch"], weight)
+
+        deriv_class = np.zeros((tree.GetEntries(),
+                                len(config_train["variables"])))
+
+        for i_event in range(tree.GetEntries()):
+            tree.GetEntry(i_event)
+
+            # Preprocessing
+            values_stacked = np.hstack(values).reshape(1, len(values))
+            values_preprocessed = preprocessing.transform(values_stacked)
+
+            # Keras inference
+            response = model_keras.predict(values_preprocessed)
+            response_keras = np.squeeze(response)
+
+            # Tensorflow inference
+            response = sess.run(
+                model_tensorflow,
+                feed_dict={
+                    inputs.placeholders: values_preprocessed
+                })
+            response_tensorflow = np.squeeze(response)
+
+            # Check compatibility
+            mean_error = np.mean(np.abs(response_keras - response_tensorflow))
+
+            if mean_error > 1e-5:
+                logger.fatal(
+                    "Found mean error of {} between Keras and TensorFlow output for event {}.".
+                    format(mean_error, i_event))
+                raise Exception
+
+            # Calculate first-order derivatives
+            deriv_values = sess.run(
+                deriv_ops[class_],
+                feed_dict={
+                    inputs.placeholders: values_preprocessed
+                })
+            deriv_values = np.squeeze(deriv_values)
+            deriv_class[i_event, :] = deriv_values
+
+        mean_abs_deriv[class_] = np.mean(np.abs(deriv_class), axis=0)
+
+    # Plotting
+    classes = config_train["classes"]
+    variables = config_train["variables"]
+    plt.figure(0, figsize=(len(variables), len(classes)))
+    axis = plt.gca()
+    matrix = np.vstack([mean_abs_deriv[class_] for class_ in classes]).T
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            axis.text(
+                i + 0.5,
+                j + 0.5,
+                '{:.2f}'.format(matrix[i, j]),
+                ha='center',
+                va='center')
+    q = plt.pcolormesh(matrix.T, cmap='Wistia')
+    #cbar = plt.colorbar(q)
+    #cbar.set_label("mean(abs(Taylor coefficients))", rotation=270, labelpad=20)
+    plt.xticks(
+        np.array(range(len(variables))) + 0.5, variables, rotation='vertical')
+    plt.yticks(
+        np.array(range(len(classes))) + 0.5, classes, rotation='horizontal')
+    plt.xlim(0, len(config_train["variables"]))
+    plt.ylim(0, len(config_train["classes"]))
+    output_path = os.path.join(config_train["output_path"],
+                               "fold{}_keras_taylor_1D.png".format(args.fold))
+    logger.info("Save plot to {}.".format(output_path))
+    plt.savefig(output_path, bbox_inches='tight')
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    config_test = parse_config(args.config_testing)
+    config_train = parse_config(args.config_training)
+    main(args, config_test, config_train)
--- training/keras_models.py
+++ training/keras_models.py
@@ -1,7 +1,8 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import *
 from keras.optimizers import *
 from keras.regularizers import l2
+import numpy as np
 
 
 def example(num_inputs, num_outputs):
@@ -141,3 +142,41 @@ def smhtt_dropout(num_inputs, num_outputs):
 
     model.compile(loss="mean_squared_error", optimizer=Nadam())
     return model
+
+def smhtt_dropout_tanh(num_inputs, num_outputs):
+    model = Sequential()
+
+    for i, nodes in enumerate([200] * 2):
+        if i == 0:
+            model.add(Dense(nodes, input_dim=num_inputs))
+        else:
+            model.add(Dense(nodes))
+        model.add(Activation("tanh"))
+        model.add(Dropout(0.5))
+
+    model.add(Dense(num_outputs))
+    model.add(Activation("softmax"))
+
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    return model
+
+def smhtt_dropout_tanh_tensorflow(input_placeholder, keras_model):
+    weights = {}
+    for layer in keras_model.layers:
+        print("Layer: {}".format(layer.name))
+        for weight, array in zip(layer.weights, layer.get_weights()):
+            print("    weight, shape: {}, {}".format(weight.name,
+                    np.array(array).shape))
+            weights[weight.name] = np.array(array)
+
+    w1 = tf.get_variable('w1', initializer=weights['dense_1/kernel:0'])
+    b1 = tf.get_variable('b1', initializer=weights['dense_1/bias:0'])
+    w2 = tf.get_variable('w2', initializer=weights['dense_2/kernel:0'])
+    b2 = tf.get_variable('b2', initializer=weights['dense_2/bias:0'])
+    w3 = tf.get_variable('w3', initializer=weights['dense_3/kernel:0'])
+    b3 = tf.get_variable('b3', initializer=weights['dense_3/bias:0'])
+
+    l1 = tf.tanh(tf.add(b1, tf.matmul(input_placeholder, w1)))
+    l2 = tf.tanh(tf.add(b2, tf.matmul(l1, w2)))
+    l3 = tf.nn.softmax(tf.add(b3, tf.matmul(l2, w3)))
+    return l3
